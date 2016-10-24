7b203908b8e06d63e930d23c94a7eae29034f988
nsholmes <metaproductions@gmail.com>
2016-10-24 01:50:18 -0500

    Updated ang2appModule.sh to import AppComponent and add declarations and bootstrap fields on the NgModule decorator

--- ang2appModule.sh
+++ ang2appModule.sh
@@ -4,8 +4,13 @@ cat > ${1} <<eof
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppComponent } from './app.component'
+
 @NgModule({
-  imports:      [ BrowserModule ]
+  imports:      [ BrowserModule ],
+  declarations: [ AppComponent ],
+  bootstrap:  [ AppComponent]
 })
 export class AppModule { }
+
 eof
